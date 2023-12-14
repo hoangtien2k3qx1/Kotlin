7804bca0c10825dcafb11c5c5e1e469991c5586b
Hoàng Anh Tiến <hoangtien2k3qx1@gmail.com>
2023-12-14 17:24:53 +0700

    update

--- Higher_Order_Function/Higher-Order-Function.kt
+++ Higher_Order_Function/Higher-Order-Function.kt
@@ -11,26 +11,24 @@ fun <T, R> Collection<T>.fold(
     return accumulator
 }
 
-
 fun main() {
 
     val items = listOf(1, 2, 3, 4, 5)
 
 // Lambdas are code blocks enclosed in curly braces.
-    items.fold(0) {
-        // When a lambda has parameters, they go first, followed by '->'
+    items.fold(0, {
             acc: Int, i: Int ->
         print("acc = $acc, i = $i, ")
         val result = acc + i
         println("result = $result")
         // The last expression in a lambda is considered the return value:
         result
-    }
+    })
 
-    // Parameter types in a lambda are optional if they can be inferred:
+// Parameter types in a lambda are optional if they can be inferred:
     val joinedToString = items.fold("Elements:", { acc, i -> acc + " " + i })
 
-    // Function references can also be used for higher-order function calls:
+// Function references can also be used for higher-order function calls:
     val product = items.fold(1, Int::times)
 
 }
